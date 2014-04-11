408a44f509968c30dab855c5a76da9c4318d7a7a
eureka <eureka_bing@163.com>
2014-04-11 17:09:29 +0800

    add Common class

---
+++ Classes/Common.h
@@ -0,0 +1,44 @@
+/*
+ * Common.h
+ *
+ *  Created on: 2014-2-11
+ *      Author: apple
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include "cocos2d.h"
+
+#define kTileMargin 20
+
+#define kLevelKey "Anagram_Level"
+#define kSoundKey "Anagram_Sound"
+#define kBestScoreKey "Anagram_BestScore"
+#define kCurScoreKey "Anagram_CurScore"
+
+//audio defines
+#define kSoundDing  "ding.mp3"
+#define kSoundWrong "wrong.m4a"
+#define kSoundWin   "win.mp3"
+
+using namespace cocos2d;
+
+namespace freemanapp{
+	class Common {
+	public:
+		static int random(int s,int e);
+		static long millisecondNow();
+
+		static int getCameraWith();
+		static void setCameraWidth(int width);
+		static int getCameraHeight();
+		static void setCameraHeight(int height);
+
+	private:
+		static int kWidth;
+		static int kHeight;
+	};
+}
+
+#endif /* COMMON_H_ */
